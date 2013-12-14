efc13463274c8f83cf3aab432fbd0830934048fe
Jonno.5000 <Jonno.5000@gmail.com>
2013-12-14 01:27:30 +0000

    Minimizer, using JUI compressor

--- build/compiler/compile.bat
+++ build/compiler/compile.bat
@@ -1,1 +1,9 @@
-start ../backend_win/builder/bin/Release/builder.exe
+@echo off
+builder.exe
+
+setlocal
+set PATH=%PATH%;../backend_win/builder/bin/Release/
+
+echo Running YUI Compressor...
+java.exe -jar yuicompressor-2.4.8pre.jar --nomunge -o ../jsbox2d.min.js ../jsbox2d.js
+endlocal
